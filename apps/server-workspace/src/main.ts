aec3d7261a1ff85e538418971493dc0678ecf3b4
wallee <920576696@qq.com>
2022-09-19 00:23:50 +0800

    feat: add filter of http and interceptor

--- apps/server-workspace/src/main.ts
+++ apps/server-workspace/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全局网络请求异常过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
+  // 全局注册拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
