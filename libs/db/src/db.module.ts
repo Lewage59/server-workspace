fa9940074d2d04627d674cf868211a5963dfcae7
wojun.li <wojun.li@shopee.com>
2022-09-18 17:43:20 +0800

    feat: add db lib

---
+++ libs/db/src/db.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbService } from './db.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root', // 自定义
+      password: 'root123', // 自定义
+      database: 'server_workspace',
+      entities: [],
+      synchronize: true,
+      charset: 'utf8mb4', // 设置chatset编码为utf8mb4
+    }),
+  ],
+  providers: [DbService],
+  exports: [DbService],
+})
+export class DbModule {}
